2f96565a9ef0691019b566f06530b6faf4b4206a
Yauheni80 <kiselevevgeniy1@gmail.com>
2022-10-04 17:38:55 -0400

    first commit
    first

---
+++ main.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+import pandas as pd
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    my_array = pd.array(['a', 'b'], dtype=str)
+    print_hi('PyCharm')
+    print(my_array)
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
