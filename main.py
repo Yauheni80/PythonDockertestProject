6fc35100dccf32d75a608b3bda40722b35d78ec9
Yauheni80 <kiselevevgeniy1@gmail.com>
2022-10-04 21:29:14 -0400

    initial commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # This is a sample Python script.
 import pandas as pd
+import numpy as np
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
@@ -12,8 +13,10 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     my_array = pd.array(['a', 'b'], dtype=str)
+    numpyArr = np.array([1, 2, 3])
     #print_hi('PyCharm')
     print(my_array)
+    print(numpyArr)
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
